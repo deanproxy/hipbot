b490bd2f41d0f7c28a66d27cbd437a2eb885d3fd
Dean Jones <dean.jones@pamlabdev.com>
2016-02-06 15:20:21 -0500

    fix help section

--- commands/help.py
+++ commands/help.py
@@ -2,7 +2,10 @@
 def show(msg, params=None):
     room = msg['room']
     room.message('Here is a list of things I can do...')
-    room.message('/code * get build status for _branch_.\n' +
-            '* alias build _name_ _buildkey_\n' + 
+    room.message('/code * alias build _name_ _buildkey_\n' + 
+            '* get build status for _buildkey_\n' +
+            '* start build for _buildkey_\n' +
+            '* watch build for _buildID_\n' +
+            '* tell me a joke\n' +
             '* die (do you really want to hurt me?).')
 
